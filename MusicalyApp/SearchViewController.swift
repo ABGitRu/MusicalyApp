6896d6a5191532cf00b917e2553e07e6d81d1810
Alexey Barinov <begoodbebad2012@gmail.com>
2021-05-18 21:03:45 +0300

    added: 1. Tableview 2. Search controller

--- MusicalyApp/SearchViewController.swift
+++ MusicalyApp/SearchViewController.swift
@@ -7,23 +7,51 @@
 
 import UIKit
 
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
+
 class SearchViewController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    let tracks = [TrackModel(trackName: "Summer Time", artistName: "Mitsu"),
+    TrackModel(trackName: "Glitch", artistName: "Mitsu")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .white
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        setupSearchBar()
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+        tracks.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
-        
-        cell.textLabel?.text = "indexpath: \(indexPath)"
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.artistName)\n\(track.trackName)"
+        cell.imageView?.image = #imageLiteral(resourceName: "psy")
+        cell.textLabel?.numberOfLines = 0
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+}
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
 }
